9a7cd5c4c7a801dd1902da7d7bceb3f8564ac0c8
Espero pueda subirlo <a2020111314@estudiantes.upsa.edu.bo>
2021-10-13 18:35:16 -0400

    MajoController agregado a Startup.cs

--- Parcial_Soliz_MJ/Naipe/Naipe.cs
+++ Parcial_Soliz_MJ/Naipe/Naipe.cs
@@ -4,16 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Parcial_Soliz_MJ.Naipes
+namespace Parcial_Soliz_MJ.Naipe
 {
     public class Naipe
     {
         [Key]
         public string NaipeId { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage ="0 es requerido")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="0 es requerido")]
         public string Nombre { get; set; }
         [Url]
-        [StringLength(100, MinimumLength =10, ErrorMessage ="La longitud debe estar entre 10 y 100")]
+        [StringLength(100, MinimumLength =10, ErrorMessage ="La longitud debe ser de 10 a 100 caracteres")]
         public string Imagen { get; set; }
     }
 }
