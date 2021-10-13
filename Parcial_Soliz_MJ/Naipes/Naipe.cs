fd446f9f86ea39a2dd2068f3a9784e24246122ff
Espero pueda subirlo <a2020111314@estudiantes.upsa.edu.bo>
2021-10-13 17:57:14 -0400

    Primer Modelo Naipes

---
+++ Parcial_Soliz_MJ/Naipes/Naipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial_Soliz_MJ.Naipes
+{
+    public class Naipe
+    {
+        [Key]
+        public string NaipeId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage ="0 es requerido")]
+        public string Nombre { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength =10, ErrorMessage ="La longitud debe estar entre 10 y 100")]
+        public string Imagen { get; set; }
+    }
+}
