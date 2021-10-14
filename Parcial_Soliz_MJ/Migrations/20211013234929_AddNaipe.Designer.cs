ac37e68761c470c18c1920ee5f42a8ce24f39cfe
Espero pueda subirlo <a2020111314@estudiantes.upsa.edu.bo>
2021-10-13 20:05:03 -0400

    Comprobado

--- Parcial_Soliz_MJ/Migrations/20211013234929_AddNaipe.Designer.cs
+++ Parcial_Soliz_MJ/Migrations/20211013234929_AddNaipe.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parcial_Soliz_MJ.Data;
 
 namespace Parcial_Soliz_MJ.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211013234929_AddNaipe")]
+    partial class AddNaipe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Parcial_Soliz_MJ/Models/Naipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial_Soliz_MJ.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public string NaipeId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "0 es requerido")]
+        public string Nombre { get; set; }
+        [Url]
+        public string Imagen { get; set; }
+    }
+}
